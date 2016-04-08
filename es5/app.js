ea2f3b97f088f1c62eada1ee1803c622dcf43d5f
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-04-07 19:29:21 -0700

    item and fighting mechanic works with messages

--- es5/app.js
+++ es5/app.js
@@ -30,29 +30,77 @@ var items = [
 
 angular.module('app', [])
   .controller('GameController', function() {
-    this.characters = 'name',
-    this.boss = {
-      // position: function(){
-      //   var randX = Math.floor(Math.random() * (4));
-      //   var randY = Math.floor(Math.random() * (4));
-      //   if(randX === curPosition[0] && randY === curPosition[1]){
-      //     console.log('fight!');
-      //   }
-      //   this.bossPosition = [randX, randY]
-      // },
-      health: 100,
-      attack: 120
-    },
+    this.message = 'none at the moment';
+    this.battleMsg = ' ';
+    this.characters = newRobot;
     this.x = xcords;
     this.y = ycords;
     this.bossPosition = bossPosition;
     this.curPosition = curPosition;
+    this.getItems = function() {
+      var ranNum = Math.floor(Math.random() * (10));
+      if (newRobot.luck) {
+        ranNum = Math.floor(Math.random() * (10 - newRobot.luck)) + newRobot.luck;
+      }
+      if(ranNum < 5) {
+        var iDelta = items[ranNum];
+        // console.log(iDelta.name);
+        if(iDelta.name === 'shield') {
+          newRobot.toughness += iDelta.buff;
+          this.message = 'You found some armor and you put it on. Toughness +10';
+        }
+        if(iDelta.name === 'bullets') {
+          newRobot.weaponStat += iDelta.buff;
+          this.message = 'You found some special bullets. This will come in handy for the boss. WeaponStat +10';
+        }
+        if(iDelta.name === 'banana peel') {
+          newRobot.batteryLvl += iDelta.buff;
+          this.message = 'You slipped on a banana peel, loose 1 battery.';
+        }
+        if(iDelta.name === 'batteries') {
+          newRobot.batteryLvl += iDelta.buff;
+          this.message = 'You found some batteries, +1 battery.';
+        }
+        if(iDelta.name === 'four leaf clover') {
+          newRobot.luck += iDelta.buff;
+          this.message = 'You found a four leaf clover, \"how did this grow underground? who really knows\". Luck +2';
+        }
+      }
+    }
+    this.bossMovement = function() {
+      var randX = Math.floor(Math.random() * (3));
+      var randY = Math.floor(Math.random() * (3));
+      if(randX === curPosition[0] && randY === curPosition[1]){
+        this.message = '!!!! You\'ve encoutered the boss !!!!!'
+        if(this.boss.attack >= this.characters.health + this.characters.toughness) {
+          this.battleMsg = 'You ran out of bullets and your armor couldn\'t stop the boss\'s attack. YOU LOSE!!'
+        }
+        if(this.characters.weaponStat >= this.boss.health) {
+          this.battleMsg = 'You overwhelmed the boss with your barrage of bullets. YOU WIN!!'
+        }
+
+      }
+      var bossCords = [randX, randY];
+      this.bossPosition = bossCords
+    };
+    this.boss = {
+      health: 50,
+      attack: 120
+    };
     this.getPosition = function(x, y) {
-      this.boss.position();
+      this.message = 'none at the moment';
+      this.characters.batteryLvl -= 1;
+      if(this.characters.batteryLvl === 0) {
+        this.message = 'Ruh Roh! You forgot your extension cable, you lose! Refresh to play again.'
+      }
+      this.getItems();
       if(x + curPosition[0] < 0 || y + curPosition[1] < 0 || x + curPosition[0] > 4 || y + curPosition[1] > 4) {
-        return console.log('you walked into a wall, you can\'t move in that direction');
+        this.characters.batteryLvl += 1;
+        this.message = 'You walked into a wall, you can\'t move in that direction!';
+        return
       }
       this.curPosition = [curPosition[0] += x, curPosition[1] += y];
+      this.bossMovement();
     }
   })
 
@@ -70,50 +118,7 @@ function RobotSoldier(toughness, batteryLvl){
   this.batteryLvl = batteryLvl;
 }
 
-var newRobot = new RobotSoldier(2, 13)
+var newRobot = new RobotSoldier(20, 13)
 newRobot.health = 50;
 newRobot.weaponStat = 20;
 newRobot.luck = 0;
-// console.log('initial robot setup, ', newRobot);
-
-game.bossMovement = function() {
-  var randX = Math.floor(Math.random() * (4));
-  var randY = Math.floor(Math.random() * (4));
-  if(randX === curPosition[0] && randY === curPosition[1]){
-    console.log('fight!');
-  }
-  return
-}
-
-game.getItems = function(luck) {
-  var ranNum = Math.floor(Math.random() * (10));
-  if (luck) {
-    ranNum = Math.floor(Math.random() * (10 - luck)) + luck;
-  }
-  // console.log(ranNum);
-  if(ranNum < 5) {
-    var iDelta = items[ranNum];
-    console.log(iDelta.name);
-    if(iDelta.name === 'shield') {
-      newRobot.toughness += iDelta.buff;
-    }
-    if(iDelta.name === 'bullets') {
-      newRobot.weaponStat += iDelta.buff;
-    }
-    if(iDelta.name === 'banana peel') {
-      newRobot.batteryLvl += iDelta.buff;
-    }
-    if(iDelta.name === 'batteries') {
-      newRobot.batteryLvl += iDelta.buff;
-    }
-    if(iDelta.name === 'four leaf clover') {
-      newRobot.luck += iDelta.buff;
-    }
-  }
-  // console.log('after getitems, ', newRobot);
-}
-
-game.position = function(x, y) {
-  // game.getItems()
-
-}
